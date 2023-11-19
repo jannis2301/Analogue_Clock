aaab1ff44d3b0197dc42c0fb6977d8d28f8e03cf
jannis2301 <jannis2301@users.noreply.github.com>
2023-11-19 19:32:20 +0000

    deploy: 1b1aa2cdde35a92e297bb87be3b950a394760c3c

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import { Clock } from './Clock'
-
-const clock = document.querySelector('.clock') as HTMLDivElement
-
-const bootstrap = () => new Clock(clock)
-
-window.addEventListener('DOMContentLoaded', () => bootstrap())
